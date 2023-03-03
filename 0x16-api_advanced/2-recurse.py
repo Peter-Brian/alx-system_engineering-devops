97499628ae29592780bd1e351e22b7124806d072
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-03-03 10:18:35 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
+"""Top ten"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """queries Reddit API and returns list containing the titles of
-    all hot articles for a given subreddit
-    Ensure that you are not following redirects.
-    Returns:
-        If no results are found for given subreddit, return None
-        If not a valid subreddit, return None.
-    """
-    url_base = 'http://www.reddit.com/r/'
-    url_query = '{:s}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'starcraft'}
-    r = requests.get(url_base + url_query, headers=headers)
+    """Return a list of titles of all hot articles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    params = {'after': after}
 
-    if (r.status_code is 302):
-        print("None")
-        return
-    if (r.status_code is 404):
-        return None
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        try:
+            data = response.json().get("data")
+            after = data.get("after", None)
+            children = data.get("children", None)
+            for each in children:
+                hot_list.append(each.get("data").get("title", None))
+        except Exception as e:
+            return None
     else:
-        r = r.json()
-        for post in r['data']['children']:
-            hot_list.append(post['data']['title'])
+        return None
+
+    if after is None:
+        if len(hot_list) == 0:
+            return None
+        return hot_list
 
     return recurse(subreddit, hot_list, after)
-
