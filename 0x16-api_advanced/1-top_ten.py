f5d1856d24e4ef2d5f90ff72eb8ceeca744d0b85
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-03-03 10:17:43 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
+"""Top ten"""
 import requests
 
 
 def top_ten(subreddit):
-    """ queries Reddit API and prints the titles of
-    first 10 hot posts listed for a given subreddit.
-    If not a valid subreddit, print None.
-    Ensure that you are not following redirects.
-    """
-    url_base = 'http://www.reddit.com/r/'
-    url_query = '{:s}/hot.json?limit={:d}'.format(subreddit, 10)
-    headers = {'user-agent': 'egsyquest'}
-    r = requests.get(url_base + url_query, headers=headers)
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if (r.status_code is 302):
-        print("None")
-        return
-    if (r.status_code is 404):
-        print("None")
-        return
+    if response.status_code == 200:
+        try:
+            data = response.json().get("data")
+            children = data.get("children")
+            for each in children[:10]:
+                print(each.get("data").get("title"))
+        except Exception as e:
+            print(None)
     else:
-        r = r.json()
-        for post in r['data']['children']:
-            print(post['data']['title'])
-
+        print(None)
